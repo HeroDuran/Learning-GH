6b340884d22bad9c92da917958cc646d07e3e43a
Gairo Duran <gairoduran23@gmail.com>
2022-09-10 11:20:59 -0700

    Initial Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Gairo Duran on 9/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 13 mini")
+    }
+}
---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Gairo Duran on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
